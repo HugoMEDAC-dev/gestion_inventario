f1b7536531bca3174df59fdd9d897485d6b2e0ec
jvt0004 <jvt0004@alu.medac.es>
2025-04-02 13:04:48 +0200

    Pantalla Login
    Interfaz login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/screens/screen_login.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -9,8 +9,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+    return MaterialApp(home: LoginRegisterScreen());
   }
 }
